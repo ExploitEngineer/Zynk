1a87586d2836174e3f3a69403bd86a00b6a9cd84
ExploitEngineer <revilwhoami0098@gmail.com>
2025-09-09 23:47:25 +0500

    feat(landing-page): add content section in landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import HeroSection from "@/components/landing-page/hero-section";
 import FeaturesSection from "@/components/landing-page/features-section";
+import ContentSection from "@/components/landing-page/content";
 
 export default function Home() {
   return (
     <>
       <HeroSection />
       <FeaturesSection />
+      <ContentSection />
     </>
   );
 }
---
+++ src/components/landing-page/content.tsx
@@ -0,0 +1,50 @@
+import { Button } from "@/components/ui/button";
+import { ChevronRight } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function ContentSection() {
+  return (
+    <section className="py-16 md:py-32">
+      <div className="relative mx-auto max-w-5xl space-y-8 px-6 md:space-y-12">
+        <div className="relative w-full h-[500px]">
+          <Image
+            className="rounded-(--radius) grayscale"
+            src="/assets/images/content.avif"
+            fill
+            objectFit="cover"
+            loading="lazy"
+            alt="team image"
+          />
+        </div>
+
+        <div className="grid gap-6 md:grid-cols-2 md:gap-12">
+          <h2 className="text-4xl font-medium">
+            Powering the Future of AI-Driven Conversations
+          </h2>
+          <div className="space-y-6">
+            <p>
+              Zynk isn't just a chat platform — it's an intelligent ecosystem
+              designed to help you build, deploy, and scale AI-powered
+              experiences. From real-time responses to context-aware insights,
+              Zynk integrates cutting-edge models with powerful APIs and
+              developer tools.
+            </p>
+
+            <Button
+              asChild
+              variant="secondary"
+              size="sm"
+              className="gap-1 pr-1.5"
+            >
+              <Link href="#">
+                <span>Learn More</span>
+                <ChevronRight className="size-2" />
+              </Link>
+            </Button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
