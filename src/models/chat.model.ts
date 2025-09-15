93971b639d3619b29af7e6f63d241f43b65112eb
ExploitEngineer <revilwhoami0098@gmail.com>
2025-09-16 03:24:57 +0500

    refactor: replace mongoose with prisma
    - Deleted `schemas/` and `models/` folders
    - Replaced `db-connection` and Mongoose setup with `lib/prisma.ts`
    - Added `prisma/schema.prisma` for database models
    - Updated database interactions to use Prisma client

--- src/models/chat.model.ts
+++
@@ -1,9 +0,0 @@
-import type { InferSchemaType } from "mongoose";
-import { model, models } from "mongoose";
-import { chatSchema } from "@/schemas/chat.schema";
-
-type ChatSchema = InferSchemaType<typeof chatSchema>;
-
-const Chat = models.Chat || model<ChatSchema>("Chat", chatSchema);
-
-export default Chat;
